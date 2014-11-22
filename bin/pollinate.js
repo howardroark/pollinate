27b6e5734eaf993aac4ea7fc69f55b7195d89b5e
Howard Roark <howie@codingcoop.org>
2014-11-22 11:20:06 -0500

    add a welcoming message

--- bin/pollinate.js
+++ bin/pollinate.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+/*  __  __    _    _  _______    ____ _   _    _    _   _  ____ _____ ____  _
+   |  \/  |  / \  | |/ / ____|  / ___| | | |  / \  | \ | |/ ___| ____/ ___|| |
+   | |\/| | / _ \ | ' /|  _|   | |   | |_| | / _ \ |  \| | |  _|  _| \___ \| |
+   | |  | |/ ___ \| . \| |___  | |___|  _  |/ ___ \| |\  | |_| | |___ ___) |_|
+   |_|  |_/_/   \_\_|\_\_____|  \____|_| |_/_/   \_\_| \_|\____|_____|____/(_)
+
+   There is a 100% chance that this project can use improvements.
+   Pull requests are ALWAYS welcome, even if just amounts to a conversation.  */
+
 var program = require('commander')
 var chalk = require('chalk')
 
